25a74f869a948a739f20505c04854d6e427cdb15
Dark-Max-Alpha <100671455+Dark-Max-Alpha@users.noreply.github.com>
2022-04-13 14:41:11 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -5,13 +5,13 @@ T-REX HIRUWA
 */
 
 function successfullMessage(msg) {
-    return "🎅❄️ T-REX*:  ```" + msg + "```"
+    return "ALPHA*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "☃️❄️ T-REX*:  ```" + msg + "```"
+    return "ALPHa*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "🎅❄️ T-REX*:  ```" + msg + "```"
+    return "ALPHA*:  ```" + msg + "```"
 }
 
 
